ffbd6b943c91e47e8f340d2350d0c6a84efa9ed1
mohansky <mohansky@gmail.com>
2025-09-09 12:35:45 +0530

    New next config commit

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,6 @@ const nextConfig = {
     '@aws-sdk/client-s3', 
     '@aws-sdk/s3-request-presigner', 
     'libsql',
-    'react-server-dom-webpack/client',
-    'react-server-dom-webpack/server',
-    'react-server-dom-webpack/server.node',
-    'react-server-dom-webpack/static',
-    'react-server-dom-turbopack/client',
-    'react-server-dom-turbopack/server',
-    'react-server-dom-turbopack/static',
-    'critters',
-    '@opentelemetry/api',
   ],
   webpack: (webpackConfig, { isServer }) => {
     webpackConfig.resolve.extensionAlias = {
@@ -33,15 +24,6 @@ const nextConfig = {
         '@smithy/smithy-client': 'commonjs @smithy/smithy-client',
         '@smithy/types': 'commonjs @smithy/types',
         'libsql': 'commonjs libsql',
-        'react-server-dom-webpack/client': 'commonjs react-server-dom-webpack/client',
-        'react-server-dom-webpack/server': 'commonjs react-server-dom-webpack/server',
-        'react-server-dom-webpack/server.node': 'commonjs react-server-dom-webpack/server.node',
-        'react-server-dom-webpack/static': 'commonjs react-server-dom-webpack/static',
-        'react-server-dom-turbopack/client': 'commonjs react-server-dom-turbopack/client',
-        'react-server-dom-turbopack/server': 'commonjs react-server-dom-turbopack/server',
-        'react-server-dom-turbopack/static': 'commonjs react-server-dom-turbopack/static',
-        'critters': 'commonjs critters',
-        '@opentelemetry/api': 'commonjs @opentelemetry/api',
       })
     }
 
