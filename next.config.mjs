c33c51daf8f1a4c100d7e12ba5ee333797b2e880
mohansky <mohansky@gmail.com>
2025-09-09 12:29:56 +0530

    New next config commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,20 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  serverExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner', 'libsql'],
+  serverExternalPackages: [
+    '@aws-sdk/client-s3', 
+    '@aws-sdk/s3-request-presigner', 
+    'libsql',
+    'react-server-dom-webpack/client',
+    'react-server-dom-webpack/server',
+    'react-server-dom-webpack/server.node',
+    'react-server-dom-webpack/static',
+    'react-server-dom-turbopack/client',
+    'react-server-dom-turbopack/server',
+    'react-server-dom-turbopack/static',
+    'critters',
+    '@opentelemetry/api',
+  ],
   webpack: (webpackConfig, { isServer }) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
@@ -20,6 +33,15 @@ const nextConfig = {
         '@smithy/smithy-client': 'commonjs @smithy/smithy-client',
         '@smithy/types': 'commonjs @smithy/types',
         'libsql': 'commonjs libsql',
+        'react-server-dom-webpack/client': 'commonjs react-server-dom-webpack/client',
+        'react-server-dom-webpack/server': 'commonjs react-server-dom-webpack/server',
+        'react-server-dom-webpack/server.node': 'commonjs react-server-dom-webpack/server.node',
+        'react-server-dom-webpack/static': 'commonjs react-server-dom-webpack/static',
+        'react-server-dom-turbopack/client': 'commonjs react-server-dom-turbopack/client',
+        'react-server-dom-turbopack/server': 'commonjs react-server-dom-turbopack/server',
+        'react-server-dom-turbopack/static': 'commonjs react-server-dom-turbopack/static',
+        'critters': 'commonjs critters',
+        '@opentelemetry/api': 'commonjs @opentelemetry/api',
       })
     }
 
